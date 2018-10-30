a21e5de92c8e6ff842161542f2cb66898f435c54
shikibum <satoshikibu@gmail.com>
2018-10-30 23:28:36 +0900

    change task

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "153.126.147.21:56310", user: "shikibum", roles: %w{web}
+server "153.126.147.21:56310", user: "shikibum", roles: %w{app db web}
 
 # role-based syntax
 # ==================
